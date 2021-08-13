de92a4053cf3a3e6412d1e9f81ff756727a6e674
Pechenyice <german.iskhakov@mail.ru>
2021-08-14 02:15:08 +0300

    added new Personal section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, {useState} from "react";
+import React from "react";
 import './App.css';
 import GreetingSection from './components/GreetingSection/GreetingSection';
+import PersonalSection from './components/PersonalSection/PersonalSection';
 
 function App() {
 
   return (
-    <section className="app">
+    <section className="app" >
       <GreetingSection />
+      <PersonalSection />
     </section>
   );
+
 }
 
 export default App;
