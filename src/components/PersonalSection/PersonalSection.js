e706cef8889a2c0126e06f88489de3891ec9d45c
Pechenyice <german.iskhakov@mail.ru>
2021-08-17 02:38:47 +0300

    added wrapper for 3d scrolling ofcourse

--- src/components/PersonalSection/PersonalSection.js
+++ src/components/PersonalSection/PersonalSection.js
@@ -31,6 +31,8 @@ function PersonalSection() {
     }, []);
 
     return (
+        <div style={{position: 'absolute', top: 0, left: 0, perspective: '10000px', width: '100%', pointerEvents: 'none'}}>
+        <div className='app__personalWrapper' style={{transform: 'translateX(150vw) translateY(150vh) translateZ(-10000px)', transition: '1s'}}>
         <section className='app__personal personal' >
 
             <div className='personal__awesomeBackground' 
@@ -44,6 +46,8 @@ function PersonalSection() {
             
 
         </section>
+        </div>
+        </div>
     );
 
 }
